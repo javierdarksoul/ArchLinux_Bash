d87719c40957ceea5e640427ec9458e8726e3609
javier <javier.rojas02@alumnos.uach.cl>
2019-09-01 07:32:03 -0400

    Update paso3.sh

--- paso3.sh
+++ paso3.sh
@@ -10,11 +10,11 @@ read -p "Ingrese SSID: " SSID
 read -p "Ingrese contraseña: " WPASS 
 sudo nmcli dev wifi connect $SSID password $WPASS
 sudo pacman -Syyu
-sudo pacman -S xorg-server xorg-xinit
-sudo pacman -S mesa
-sudo pacman –S gnome
-sudo pacman –S gnome-extra
-sudo pacman -S gdm
+sudo pacman -S --noconfirm xorg-server xorg-xinit
+sudo pacman -S --noconfirm mesa
+sudo pacman –S --noconfirm gnome
+sudo pacman –S --noconfirm gnome-extra
+sudo pacman -S --noconfirm gdm
 sudo systemctl enable gdm.service
 echo "Reiniciando en 5 seg"
 sleep 5
